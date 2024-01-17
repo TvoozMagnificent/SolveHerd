b599c05e265b1232e52bd8fcfcf29b4ca54c01ad
TvoozMagnificent <luchang1106@icloud.com>
2024-01-17 12:04:50 +0800

    Start project

---
+++ square.py
@@ -0,0 +1,8 @@
+
+class Square:
+    '''
+    Base class for all tiles on the grid.
+
+    :param int x: x position of square (horizontal, top to bottom)
+    :param int y: y position of square (vertical, left to right)
+    '''
